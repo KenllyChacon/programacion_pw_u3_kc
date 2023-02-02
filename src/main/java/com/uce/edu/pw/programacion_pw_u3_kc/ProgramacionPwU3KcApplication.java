b09441b0c42d8818cd4c918664fec07d42c1d511
KenllyChacon <klchacon@uce.edu.ec>
2023-02-01 19:43:47 -0500

    Initial commit

---
+++ src/main/java/com/uce/edu/pw/programacion_pw_u3_kc/ProgramacionPwU3KcApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.pw.programacion_pw_u3_kc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramacionPwU3KcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramacionPwU3KcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uce/edu/pw/programacion_pw_u3_kc/ProgramacionPwU3KcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu.pw.programacion_pw_u3_kc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramacionPwU3KcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
