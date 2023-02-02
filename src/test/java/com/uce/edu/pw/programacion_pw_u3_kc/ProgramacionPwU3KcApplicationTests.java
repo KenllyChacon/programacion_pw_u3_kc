6f4b70db1b8a3615bab7c1ecda2600894bf14b29
KenllyChacon <klchacon@uce.edu.ec>
2023-02-01 19:44:17 -0500

    conexion a base de datos

--- src/test/java/com/uce/edu/pw/programacion_pw_u3_kc/ProgramacionPwU3KcApplicationTests.java
+++ src/test/java/com/uce/edu/pw/programacion_pw_u3_kc/ProgramacionPwU3KcApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProgramacionPwU3KcApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
